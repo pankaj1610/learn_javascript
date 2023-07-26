065145fd288049b10ed3f19a49ad71874b03f09b
pankaj1610 <pankajsha584@gmail.com>
2023-07-26 19:03:01 +0530

    Higher order function

---
+++ HigerOrderFunction.js
@@ -0,0 +1,15 @@
+/**
+ * A function which takes a function as an argument or return a function is known as Higher order function.
+ */
+
+
+function x() {
+    
+}
+
+// y is a higher order function and x is a call back function.
+function y(x) {
+    x();
+}
+
+y(x);
